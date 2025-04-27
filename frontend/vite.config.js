7551d6b2f82f926a595b8fcc63834ddf1db6c822
Shashank Lakhera <lakherashashank70@gmail.com>
2025-04-27 21:19:21 +0530

    Allowed Host - render.com

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: process.env.PORT || 10000, // Port that Render gives you (or any valid port for local dev)
+    allowedHosts: ['stoxie-frontend.onrender.com'],
     host: '0.0.0.0', // Make sure it's set to '0.0.0.0' to be publicly accessible
   },
 })
